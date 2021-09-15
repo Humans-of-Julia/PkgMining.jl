0d4118abfd02cb28a25889fa95e5a1ed457135ed
Ricardo Rosa <rmsrosa@gmail.com>
2021-09-15 18:02:35 -0300

    update temporary pkg_exploring script

--- scripts/pkg_exploring.jl
+++ scripts/pkg_exploring.jl
@@ -7,21 +7,32 @@ using .PkgMining
 
 df = pkg_mining()
 
-df[df.owner .== "SciML", :]
+df[df.owner .== "JuliaImages", :name]
+
+df[df.name .== "DifferentialEquations", :owner]
 
 df[occursin.("holy", df.owner), :].name
 
 gd = groupby(df, :owner)
 
-owners = Dict(first(g.owner) => size(g, 1) for g in gd)
-owners = [first(g.owner) => size(g, 1) for g in gd]
+owners = Dict(first(g.owner) => nrow(g) for g in gd)
+owners = [first(g.owner) => nrow(g) for g in gd]
 
 # Ratio of owners with 10 or more packages:
 length(filter(x -> last(x) ≥ 10, owners))/length(owners)
 
 sort!(owners, by = x -> last(x), rev = true)
 
-[size(df[df.owner .== owner, :], 1) for owner in df.owner]
+[nrow(df[df.owner .== owner, :]) for owner in df.owner]
+
+# Create DataFrame with owner and the number of packages they own
+df_owned = combine(groupby(df, :owner), nrow => :pkgs_owned)
+
+# Copy and extend the packages DataFrame to contain column with the number of packages
+# owned by the onwer of that package
+df_copy = copy(df)
+
+transform(groupby(df_copy, :owner), :owner => length => :pkgs_owned)
 
 # Check total number of packages
 @info sum(unique(df, :owner).pkgs_owned) == size(df, 1)
