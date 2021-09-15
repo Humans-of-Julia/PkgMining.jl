a1b259e5bad579c0a96ff943773808f836294354
Ricardo Rosa <rmsrosa@gmail.com>
2021-09-15 18:03:30 -0300

    add columns for first & last version and sha1

--- src/PkgMining.jl
+++ src/PkgMining.jl
@@ -14,18 +14,35 @@ function pkg_mining()
         name = String[],
         uuid = String[],
         owner = String[],
-        pkgs_owned = Int[],
-        repo = String[]
+        firstversion = String[],
+        lastversion = String[],
+        repo = String[],
+        sha1 = String[]
     )
 
     @showprogress "Mining $(length(registry["packages"])) packages: " for (uuid, info) in
                                                                         registry["packages"]
         pkg = TOML.parsefile(joinpath(general, info["path"], "Package.toml"))
+        versions = TOML.parsefile(joinpath(general, info["path"], "Versions.toml"))
+        firstversion, lastversion = string.(extrema(VersionNumber.(keys(versions))))
+        sha1 = versions[lastversion]["git-tree-sha1"]
         owner = split(pkg["repo"], '/')[end-1]
-        push!(df, (info["name"], uuid, owner, 0, pkg["repo"]))
-    end
+        push!(
+            df,
+            (
+                info["name"],
+                uuid,
+                owner,
+                0,
+                firstversion,
+                lastversion,
+                pkg["repo"],
+                sha1
+            )
+        )
 
-    df.pkgs_owned = [size(df[df.owner .== owner, :], 1) for owner in df.owner]
+    end
+    
     return df
 end
 
